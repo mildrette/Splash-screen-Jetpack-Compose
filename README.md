1366bff1680314febc71d7fe33d939306ec36886
mildrette <mildredfonka1@maild.com>
2025-10-24 10:12:53 +0100

    add and made a splashscreen or you can call it welcome screen work

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# TopNav
-Creating navbar in jetpack compose
+# Splash Screen or Welcome screen of an app
+
+A splash screen is the first screen that appears when you open an app 
+usually displaying the app logo, name, or branding 
+while the app is loading or initializing in the background.
+
+### Purpose of a Splash Screen
+
+### Branding:
+Shows your logo or app name to make a good first impression.
+
+### Loading Time Masking:
+Covers up short delays while your app is setting up resources.
+
+### Smooth Launch Transition:
+Makes the startup feel professional and seamless instead of showing a blank or flickering screen.
---
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.splashscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.splashscreen.ui.theme.SplashScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            SplashScreenTheme {
+               Box(modifier = Modifier.fillMaxSize(),
+                   contentAlignment = Alignment.Center) {
+Greeting("Welcome to this App")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(message: String){
+    Text(message)
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SplashScreenTheme {
+        Greeting("Welcome to this App")
+    }
+}
